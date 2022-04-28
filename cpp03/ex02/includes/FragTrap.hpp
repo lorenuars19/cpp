5fb66441bb503a6769e5dc0714aedc56078c48c1
lorenuars19 <lorenuar.s19@gmail.com>
2022-04-28 17:20:13 +0200

    ex02

--- cpp03/ex02/includes/FragTrap.hpp
+++ cpp03/ex02/includes/FragTrap.hpp
@@ -1,33 +1,30 @@
-#ifndef FORTYTWO_HPP
-#define FORTYTWO_HPP
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
 
+#include "ClapTrap.hpp"
 #include <iostream>
 
-class FortyTwo
+class FragTrap : public ClapTrap
 {
 public:
-	typedef FortyTwo t;
+	typedef FragTrap t;
 	// ----------------------------- Constructors ------------------------------ //
-	FortyTwo();			  // Default Constructor
-	FortyTwo(int var);	  // Fields Constructor
-	FortyTwo(const t &c); // Copy Constructor
+	FragTrap();					// Default Constructor
+	FragTrap(std::string name); // Fields Constructor
+	FragTrap(const t &c);		// Copy Constructor
 
 	// ------------------------------ Destructor ------------------------------- //
-	~FortyTwo(); // Destructor
+	~FragTrap(); // Destructor
 
 	// ------------------------------- Operators ------------------------------- //
-	FortyTwo &operator=(const t &a);
+	FragTrap &operator=(const t &a);
 	// Copy Assignement Operator
 
 	// --------------------------- Getters && Setters -------------------------- //
-	int get_var() const;
-	void set_var(int input);
-
 	// --------------------------------- Methods ------------------------------- //
-	int is_equal(const FortyTwo comp);
+	void highFiveGuys();
 
 private:
-	int var;
 };
 
 #ifndef NO_DEBUG
@@ -36,19 +33,19 @@ private:
 #define _ARG(arg) #arg << "(" << arg << ") "
 #endif /* _ARG */
 
-#define _FORTYTWO_ARGS _ARG(var)
-#define _FORTYTWO_AUTO(COLOR_Cvar TEXT)                         \
+#define _FRAGTRAP_ARGS ""
+#define _FRAGTRAP_AUTO(COLOR_CODE, TEXT)                        \
 	std::cout << "{ \e[" << COLOR_CODE << ";1m"                 \
 			  << TEXT << " " << __PRETTY_FUNCTION__ << "\e[0m " \
 			  << "[\033[34;47m" << this                         \
-			  << "\033[0m]\033[0m: " << _FORTYTWO_ARGS          \
+			  << "\033[0m]\033[0m: " << _FRAGTRAP_ARGS          \
 			  << "}" << std::endl;
 #else
 
-#define _FORTYTWO_AUTO(x, y) ;
-#define _FORTYTWO_ARGS ""
+#define _FRAGTRAP_AUTO(x, y) ;
+#define _FRAGTRAP_ARGS ""
 #define _ARG ""
 
 #endif /* NO_DEBUG */
 
-#endif /* FORTYTWO_HPP */
+#endif /* FRAGTRAP_HPP */

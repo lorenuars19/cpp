2a0d93d775135dfbf6983ee96d1c6268cd4bfb48
lorenuar.s19 <lorenuar.s19@gmail.com>
2022-05-11 17:27:10 +0200

    ex00

---
+++ cpp07/ex00/templ.hpp
@@ -0,0 +1,16 @@
+#ifndef TEMPL_HPP
+#define TEMPL_HPP
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+void swap(T &a, T &b);
+
+template <typename T>
+T min(T a, T b);
+
+template <typename T>
+T max(T a, T b);
+
+#endif

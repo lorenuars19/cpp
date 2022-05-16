a4cbe342d479a0e3b438b9090bac00cf22e884a3
lorenuar.s19 <lorenuar.s19@gmail.com>
2022-05-16 19:41:12 +0200

    ex00 cpp8

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -10,22 +10,20 @@ class ItemNotFoundException : public std::exception
 public:
 	const char *what() const throw()
 	{
-		return "\e[31;1mError: Item not found\e[0m\n";
+		return "\e[31;1mError: Item not found\e[0m";
 	}
 };
 
 template <class T>
-int &easyfind(const T &cont, int num)
+typename T::iterator easyfind(T &cont, int num)
 {
-	for (std::iterator<std::input_iterator_tag, T> it = cont.begin();
-		 it != cont.end();
-		 std::advance(it, 1))
+	for (typename T::iterator it = cont.begin();
+		 it != cont.end(); ++it)
 	{
-		std::cout << "it :" << *it << '\n';
-		// if (it == num)
-		// {
-		// 	return (&num);
-		// }
+		if (*it == num)
+		{
+			return (it);
+		}
 	}
 	throw ItemNotFoundException();
 }
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,25 +1,54 @@
 #include "easyfind.hpp"
+#include <iostream>
+#include <list>
+#include <vector>
 
 int main(void)
 {
-	std::vector<int> vi;
+	std::vector<int> vec;
+	std::vector<int>::iterator vec_it;
 
-	for (int i = 0; i < 50; i += 2)
+	std::cout << "\nstd::vector<int> :\n";
+
+	for (int i = 0; i < 6; i += 2)
 	{
-		vi.push_back(i);
+		vec.push_back(i);
+		std::cout << " vec.push_back(" << i << ");\n";
 	}
-
-	int ret;
-
+	std::cout << "\neasyfind(vec, 5) :\n";
 	try
 	{
-		ret = easyfind(vi, 41);
+		vec_it = easyfind(vec, 5);
 	}
 	catch (const std::exception &e)
 	{
 		std::cerr << e.what() << '\n';
 	}
+	std::cout << "\neasyfind(vec, 4) :\n";
 
-	ret = easyfind(vi, 42);
-	std::cout << "ret : " << ret << '\n';
+	vec_it = easyfind(vec, 4);
+	std::cout << "vec_it : " << *vec_it << '\n';
+
+	std::list<int> lst;
+	std::list<int>::iterator lst_it;
+
+	std::cout << "\nstd::list<int> :\n";
+	for (int i = 0; i < 6; i += 2)
+	{
+		lst.push_back(i);
+		std::cout << " lst.push_back(" << i << ");\n";
+	}
+	std::cout << "\neasyfind(lst, 5) :\n";
+	try
+	{
+		lst_it = easyfind(lst, 5);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	std::cout << "\neasyfind(lst, 4) :\n";
+
+	lst_it = easyfind(lst, 4);
+	std::cout << "lst_it : " << *lst_it << '\n';
 }

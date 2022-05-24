caebc080d8707ae5214324a3749fb5f42a95d496
lorenuars19 <lorenuar.s19@gmail.com>
2022-05-24 16:58:07 +0200

    cpp08 DONE

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -2,6 +2,18 @@
 int main()
 {
 	Span sp = Span(5);
+
+
+	try
+	{
+		std::cout << sp.shortestSpan() << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
+
+
 	sp.addNumber(6);
 	sp.addNumber(3);
 	sp.addNumber(17);
@@ -10,5 +22,14 @@ int main()
 
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
+
+	try
+	{
+		sp.addNumber(42);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
 	return 0;
 }

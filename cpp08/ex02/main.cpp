cfb5c8f9d2416b6b6091adc4514127fc1ee1adae
lorenuars19 <lorenuar.s19@gmail.com>
2022-05-25 12:35:07 +0200

    cpp ALL DONE

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -2,43 +2,47 @@
 
 #include "MutantStack.hpp"
 
-int main() {
-  MutantStack<int> mstack;
-  mstack.push(5);
-  mstack.push(17);
-  std::cout << "Stack top : " << mstack.top() << std::endl;
-  mstack.pop();
-  std::cout << "Stack size : " << mstack.size() << std::endl;
-  mstack.push(3);
-  mstack.push(5);
-  mstack.push(737);
-  mstack.push(0);
-  MutantStack<int>::iterator it = mstack.begin();
-  MutantStack<int>::iterator ite = mstack.end();
-  ++it;
-  --it;
-  std::cout << "Stack size : " << mstack.size() << std::endl;
-  while (it != ite) {
-    std::cout << "Iterator : " << *it << std::endl;
-    ++it;
-  }
-  std::stack<int> s(mstack);
+int
+main()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << "Stack top : " << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << "Stack size : " << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	std::cout << "Stack size : " << mstack.size() << std::endl;
+	while (it != ite)
+	{
+		std::cout << "Iterator : " << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
 
-  std::cout << "====== STRING STACK =====" << std::endl;
+	std::cout << "====== STRING STACK =====" << std::endl;
 
-  MutantStack<std::string> sstack;
+	MutantStack<std::string> sstack;
 
-  sstack.push("Hello");
-  sstack.push("Hello-");
-  sstack.push("Hello--");
-  sstack.push("Hello---");
-  sstack.push("Hello----");
-  sstack.push("Hello-----");
-  MutantStack<std::string>::iterator sit = sstack.begin();
-  MutantStack<std::string>::iterator site = sstack.end();
-  std::cout << "String Stack size : " << sstack.size() << std::endl;
-  while (sit != site) {
-    std::cout << "Iterator : " << *sit << std::endl;
-    ++sit;
-  }
+	sstack.push("Hello");
+	sstack.push("Hello-");
+	sstack.push("Hello--");
+	sstack.push("Hello---");
+	sstack.push("Hello----");
+	sstack.push("Hello-----");
+	MutantStack<std::string>::iterator sit = sstack.begin();
+	MutantStack<std::string>::iterator site = sstack.end();
+	std::cout << "String Stack size : " << sstack.size() << std::endl;
+	while (sit != site)
+	{
+		std::cout << "Iterator : " << *sit << std::endl;
+		++sit;
+	}
 }
